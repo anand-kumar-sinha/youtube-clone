b41f16e8068ea020ec9f1355bc877843c9853c38
anandkumar1 <anandkumar7368970125@gmail.com>
2023-08-14 00:54:32 +0530

    .

--- src/App.js
+++ src/App.js
@@ -5,24 +5,28 @@ import Header from "./components/Header";
 import Feed from "./components/Feed";
 import SearchResult from "./components/SearchResult";
 import VideoDetails from "./components/VideoDetails";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
-    <AppContext>
-      <BrowserRouter>
-        <div className="flex flex-col h-full">
-          <Header />
-          <Routes>
-            <Route exact path="/" element={<Feed />} />
-            <Route
-              path="/searchResult/:searchQuery"
-              element={<SearchResult />}
-            />
-            <Route path="/video/:id" element={<VideoDetails />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </AppContext>
+    <>
+      <AppContext>
+        <BrowserRouter>
+          <div className="flex flex-col h-full">
+            <Header />
+            <Routes>
+              <Route exact path="/" element={<Feed />} />
+              <Route
+                path="/searchResult/:searchQuery"
+                element={<SearchResult />}
+              />
+              <Route path="/video/:id" element={<VideoDetails />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </AppContext>
+      <Analytics />
+    </>
   );
 };
 
